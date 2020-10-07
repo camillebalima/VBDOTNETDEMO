6969d57c66ba66581bfc50cc06668ecce6a18c5a
camillebalima <48718455+camillebalima@users.noreply.github.com>
2020-10-07 09:55:28 -0300

    Insert Done, Update Begining

---
+++ VBDemo/VBDemo/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim connection As New SqlConnection("Server=localhost; Database=VBDB; Integrated Security=true ")
+
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+        Dim sql As String = "INSERT INTO [User] (FirstName,LastName,Age) VALUES('" & txtFirstName.Text & "', '" & txtLastName.Text & "', '" & txtAge.Text & "')"
+
+        ExecuteQuery(sql)
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim sql As String = "UPDATE [User] SET FirstName = '" & txtFirstName.Text & "',LastName = '" & txtLastName.Text & "', Age = '" & txtAge.Text & "' WHERE ID = '" & txtID.Text & "'"
+
+        ExecuteQuery(sql)
+    End Sub
+
+
+#Region "Methods"
+    Public Sub ExecuteQuery(query As String)
+        Dim command As New SqlCommand(query, connection)
+        connection.Open()
+        command.ExecuteNonQuery()
+        connection.Close()
+    End Sub
+
+
+#End Region
+
+End Class
