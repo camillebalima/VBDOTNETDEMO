a3b3f8b782b32d9113b4f183322d6f250c6a1fd1
camillebalima <48718455+camillebalima@users.noreply.github.com>
2020-10-07 10:13:17 -0300

    Basic CRUD, No Validation

--- VBDemo_V1/VBDemo/Form1.vb
+++ VBDemo_V1/VBDemo/Form1.vb
@@ -6,15 +6,26 @@ Public Class Form1
 
     Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
         Dim sql As String = "INSERT INTO [User] (FirstName,LastName,Age) VALUES('" & txtFirstName.Text & "', '" & txtLastName.Text & "', '" & txtAge.Text & "')"
-
         ExecuteQuery(sql)
+
+        MessageBox.Show("Created Succesfuly")
+
     End Sub
 
     Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
         Dim sql As String = "UPDATE [User] SET FirstName = '" & txtFirstName.Text & "',LastName = '" & txtLastName.Text & "', Age = '" & txtAge.Text & "' WHERE ID = '" & txtID.Text & "'"
-
         ExecuteQuery(sql)
+
+        MessageBox.Show("Updated Succesfuly")
     End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim sql As String = "DELETE FROM [User] WHERE ID = '" & txtID.Text & "'"
+        ExecuteQuery(sql)
+
+        MessageBox.Show("Deleted Succesfuly")
+    End Sub
+
 
 
 #Region "Methods"
@@ -24,6 +35,7 @@ Public Class Form1
         command.ExecuteNonQuery()
         connection.Close()
     End Sub
+
 
 
 #End Region
